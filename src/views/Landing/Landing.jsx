1d68e1ec4fc174d3d1b1afde72549ddd007a39a5
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-09-27 16:39:39 -0300

    [Add] funcionabilidades en Landing

--- src/views/Landing/Landing.jsx
+++ src/views/Landing/Landing.jsx
@@ -1,9 +1,16 @@
-// import style from "./Landing.module.css"
+import { useNavigate } from "react-router-dom";
+
+import style from "./Landing.module.css";
 
 const Landing = () => {
+  const navigate = useNavigate();
+  const handleNavigator = () => {
+    navigate("/home");
+  };
   return (
-    <div>
+    <div className={style.landingContainer}>
       <h1>Landing</h1>
+      <button onClick={handleNavigator}>Comenzar</button>
     </div>
   );
 };
