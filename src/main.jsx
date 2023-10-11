c32d55bfdf2a3a77359835282f58356aef987fa1
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-10-10 21:02:03 -0300

    .

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import { Provider } from "react-redux";
 import store from "./redux/store/store.js";
 import axios from "axios";
 
- axios.defaults.baseURL = "http://localhost:3001";
-//axios.defaults.baseURL = "https://pokeapi-sandy.vercel.app";
+// axios.defaults.baseURL = "http://localhost:3001";
+axios.defaults.baseURL = "https://pokeapi-sandy.vercel.app";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
