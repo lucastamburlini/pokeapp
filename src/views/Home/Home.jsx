c8d21902f5909c6368f411d82c46199107ae3a48
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-11-20 17:35:11 -0300

    add loading home

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,29 +1,44 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import { getPokemons } from "../../redux/actions/actions";
 import CardContainer from "../../components/CardContainer/CardContainer";
 import SearchBar from "../../components/SearchBar/SearchBar";
 import Filters from "../../components/Filters/Filters";
 import Footer from "../../components/Footer/Footer";
+import Loader from "../../components/Loader/Loader";
 
 import style from "./Home.module.css";
 
 const Home = () => {
   const dispatch = useDispatch();
-  const pokemons = useSelector((state) => state.pokemons);
+  const [loader, setLoader] = useState(true);
 
   useEffect(() => {
-    if (pokemons.length === 0) {
-      dispatch(getPokemons());
-    }
-  }, [dispatch, pokemons]);
+    setLoader(true);
+
+    dispatch(getPokemons())
+      .then(() => {
+        setLoader(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching pokemons:", error);
+        setLoader(false);
+      });
+  }, [dispatch]);
 
   return (
     <>
       <div className={style.homeContainer}>
         <SearchBar />
         <Filters />
-        <CardContainer />
+        {loader ? (
+          <div className={style.loaderContainer}>
+            <p>Loading pokemons...</p>
+            <Loader />
+          </div>
+        ) : (
+          <CardContainer />
+        )}
       </div>
       <Footer />
     </>
